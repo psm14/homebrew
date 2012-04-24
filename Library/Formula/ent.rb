69f8155d9d0e0e933d78dfb7a5204b85a7015045
Patrick McLaughlin <psm14case@gmail.com>
2012-04-23 23:57:36 -0400

    Ent formula added

---
+++ Library/Formula/ent.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Ent < Formula
+	version '1.0'
+  homepage 'http://www.fourmilab.ch/random/'
+  url 'http://www.fourmilab.ch/random/random.zip'
+  md5 '8104a83af1ea5b280da96c92da18eae4'
+
+  def install
+    system "make"
+		bin.install "ent"
+
+    # Used in the below test
+    prefix.install "entest.mas"
+    prefix.install "entitle.gif"
+  end
+
+  def test
+    # TODO: patch entest.bat (which is also a valid unix shell script) to handle
+    # the homebrew prefix directory and also run diff 
+    return False
+  end
+end
