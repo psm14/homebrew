943c4e746963bd982a949f401e947798d045268a
Patrick McLaughlin <psm14case@gmail.com>
2012-04-24 00:02:05 -0400

    PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!

--- Library/Formula/ent.rb
+++ Library/Formula/ent.rb
@@ -1,8 +1,5 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Ent < Formula
 	version '1.0'
   homepage 'http://www.fourmilab.ch/random/'
